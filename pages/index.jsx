1619f1e90412f5c7f475ae9cce64197176adf518
TextError <savinraul1983@yahoo.com>
2021-06-24 13:09:42 +0100

    getStaticProps options

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,16 @@ export const getStaticProps = async () => {
   const toParse = await fs.readFile(filePath);
   const data = JSON.parse(toParse);
 
+  if(!data) return { redirect: { destination: '/no-data' }};
+  if(data.products.length === 0) return { notFound: true }; // returns as 404 page
+
   return {
     props: {
       ...data
     },
-    revalidate: 1
+    revalidate: 1,
+    // notFound: true
+    // redirect: { destination: '/no-data' }
   }
 };
 
