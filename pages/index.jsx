625b9a9d2b4da5c0faa5a97f84d9cbe096c91620
TextError <savinraul1983@yahoo.com>
2021-06-24 12:17:01 +0100

    getStaticProps + fs

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,20 @@
+import path from 'path';
+import fs from 'fs/promises';
 
-const Home = () => (
-  <>home</>
+const Home = ({ products }) => (
+  <>{console.log(products)}</>
 );
+
+export const getStaticProps = async () => {
+  const filePath = path.join(process.cwd(), 'data', 'backend.json');
+  const toParse = await fs.readFile(filePath);
+  const data = JSON.parse(toParse);
+
+  return {
+    props: {
+      ...data
+    }
+  }
+};
 
 export default Home;
